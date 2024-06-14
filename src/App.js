90ed7efe9c6b802ea5a310194879afc6b5c5f94f
AYUSH <143839181+iyuspandey@users.noreply.github.com>
2024-06-14 18:57:01 +0530

    added files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+
+import Log from './COMPONENT/login';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    
+   <div className="container">
+ 
+   </div>
+   <Log/>  
+   </> 
+  );
+}
+
+export default App;
+
